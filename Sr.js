a7489bbb6e599249f4dc95f61a82ff4b3747d64b
DanielleK02 <44614375+DanielleK02@users.noreply.github.com>
2019-10-10 14:22:19 -0400

    Update Sr.js

--- Sr.js
+++ Sr.js
@@ -2,11 +2,11 @@ function Sr(x, y) {
   this.x = x;
   this.y = y;
   this.r = 20;
-  
+  this.active = true
   
   this.show = function() {
     fill(255)
   ellipse(this.x, this.y, this.r*2, this.r*2)
 }
-  
+   
 }
