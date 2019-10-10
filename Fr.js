ec2aa0d5eb1329a59616678f4d23a49c9609153b
DanielleK02 <44614375+DanielleK02@users.noreply.github.com>
2019-10-10 14:22:04 -0400

    Update Fr.js

--- Fr.js
+++ Fr.js
@@ -2,11 +2,16 @@ function Fr(x, y) {
   this.x = x;
   this.y = y;
   this.r = 20;
+  this.active = true;
+
   
   
   this.show = function() {
     fill(255)
   ellipse(this.x, this.y, this.r*2, this.r*2)
 }
-  
+   this.die = function() {
+   this.active = false 
+  }
+   
 }
