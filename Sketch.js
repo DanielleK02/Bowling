980993bbcdeacf6d0950ba95d12771d9db082ab5
DanielleK02 <44614375+DanielleK02@users.noreply.github.com>
2019-12-09 13:03:55 -0500

    Update Sketch.js

--- Sketch.js
+++ Sketch.js
@@ -7,156 +7,84 @@ var score = 0;
 
 function setup() {
   createCanvas(400, 400);
-    ball = nevar ball;
-var brs = [];
-var mrs = [];
-var srs = [];
-var frs = [];
-var score = 0;
-
-function setup() {
-  createCanvas(400, 400);
     ball = new Ball()
     for (var i = 0; i < 4; i++) {
-  brs[i] = new Br(i*50+115, 30);
+  brs[i] = new Br(i*35+135, 30);
 }
-   for (var i = 0; i < 3; i++) {
-  mrs[i] = new Mr(i*50+140, 80);
+  for (var j = 0; j < 3; j++) {
+    mrs[j] = new Mr(j*35+155, 75);
   }
-   for (var i = 0; i < 2; i++) {
-  srs[i] = new Sr(i*50+165, 130);
+   
+  for (var h = 0; h < 2; h++) {
+    srs[h] = new Sr(h*35+175, 115);
   }
-  for (var i = 0; i < 1; i++) {
-  frs[i] = new Fr(i*50+190, 180);
+  
+  for (var l = 0; l < 1; l++) {
+   frs[l] = new Fr(l*35+195, 155); 
   }
 }
  
 function draw() {
   background(50, 0, 10);
-   ball.show();
+  ball.show();
   ball.update();
-  
-  
+   
   for (var i = 0; i < brs.length; i++) {
     if (brs[i].active) {
         brs[i].show();
-      }
+      
         if (ball.hits(brs[i])) {
           brs[i].die();
-          score += 1;
-
+            score++
   }
    }
+  }
   
-  for (var i = 0; i < mrs.length; i++) {
-    if(mrs[i].active) {
-       mrs[i].show();
-    }
-      if (ball.hits(mrs[i])) {
-       mrs[i].die(); 
-       score += 1;
+  for (var j = 0; j < mrs.length; j++) {
+    if(mrs[j].active) {
+       mrs[j].show();
+    
+      if (ball.hits(mrs[j])) { 
+        mrs[j].die();
+          score++
       }
   }
+  }
   
-  for (var i = 0; i < srs.length; i++) {
-   if (srs[i].active) {
-     srs[i].show(); 
-   }
-    if (ball.hits(srs[i])) {
-      srs[i].die();
-      score += 1;
+  for (var h = 0; h < srs.length; h++) {
+   if (srs[h].active) {
+     srs[h].show(); 
+   
+    if (ball.hits(srs[h])) {
+      srs[h].die();
+         score++
     }
   }
+  }
   
-  for (var i = 0; i < frs.length; i++) {
-  if (frs[i].active) {
-     frs[i].show();
-     }
-    if (ball.hits(frs[i])) {
-     frs[i].die();
-      score += 1;
-   }
+  for (var l = 0; l < frs.length; l++) {
+  if (frs[l].active) {
+     frs[l].show();
      
-  }
-  
-  fill(255)
-  textSize(20)
-  text("Score: " + score, 20, 380 )
-
-}
-
-
-
-function keyPressed() {
-  if (key === ' ') {
-   ball.move(0, -1); 
-  }
-  
-  if (keyCode === RIGHT_ARROW) {
- ball.move(1, 0); 
-  } else if (keyCode === LEFT_ARROW) {
-    ball.move(-1, 0);
-  }
-  
-  function keyReleased() {
-   ball.stop(); 
-    
-  }
-} Ball()
-    for (var i = 0; i < 4; i++) {
-  brs[i] = new Br(i*50+115, 30);
-}
-   for (var i = 0; i < 3; i++) {
-  mrs[i] = new Mr(i*50+140, 80);
-  }
-   for (var i = 0; i < 2; i++) {
-  srs[i] = new Sr(i*50+165, 130);
-  }
-  for (var i = 0; i < 1; i++) {
-  frs[i] = new Fr(i*50+190, 180);
-  }
-}
- 
-function draw() {
-  background(50, 0, 10);
-   ball.show();
-  ball.update();
-  
-  
-  for (var i = 0; i < brs.length; i++) {
-  brs[i].show();
-  }
-  
-  for (var i = 0; i < mrs.length; i++) {
-  mrs[i].show();
-  }
-  
-  for (var i = 0; i < srs.length; i++) {
-  srs[i].show();
-  }
-  
-  for (var i = 0; i < frs.length; i++) {
-  if (frs[i].active) {
-     frs[i].show();
-     }
-    if (ball.hits(frs[i])) {
-     frs[i].die();
+    if (ball.hits(frs[l])) {
+      frs[l].die();
+         score++
+    }
    }
-    
   }
   
   fill(255)
   textSize(20)
   text("Score: " + score, 20, 380 )
-
 }
 
 
 
 function keyPressed() {
   if (key === ' ') {
-   ball.move(0, -1); 
+   ball.move(0, -1);
   }
+  
   
   if (keyCode === RIGHT_ARROW) {
  ball.move(1, 0); 
@@ -164,8 +92,9 @@ function keyPressed() {
     ball.move(-1, 0);
   }
   
-  function keyReleased() {
-   ball.stop(); 
+  
+}
+function keyReleased() {
+   //ball.stop(); 
     
   }
-}
