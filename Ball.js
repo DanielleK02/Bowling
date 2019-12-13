e64783ca49f47c4e67db6eac46c51ecd2583cd0e
DanielleK02 <44614375+DanielleK02@users.noreply.github.com>
2019-12-13 14:15:52 -0500

    Update Ball.js

--- Ball.js
+++ Ball.js
@@ -1,4 +1,33 @@
- this.move = function(dx, dy) {
+function Ball() {
+  this.x = width / 2;
+  this.y = height - 60;
+  this.vx = 0;
+  this.vy = 0;
+  this.SPEED = 1.5;
+  this.active = true;
+  this.r = 40
+  this.throws = 2
+
+
+  this.show = function() {
+    fill(5);
+    ellipseMode(CENTER);
+    ellipse(this.x, this.y, 70, 70);
+  }
+
+  this.hits = function(fr) {
+    var d = dist(this.x, this.y, fr.x, fr.y);
+    if (d < this.r + fr.r) {
+      return true;
+    } else {
+      return false;
+    }
+
+  }
+
+
+
+  this.move = function(dx, dy) {
 
     this.vx = dx * this.SPEED
     this.vy = dy * this.SPEED
@@ -8,7 +37,7 @@
 
     this.x += this.vx
     this.y += this.vy
-    if (this.y < 0) {
+    if (this.y < 10) {
       //pg.reset();
       this.x = width / 2
      if (this.throws > 0) {
